4cfe4bbac7e710f4c331c1042669e64bac3bc8ef
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2024-12-15 20:24:56 +0400

    firstTask

---
+++ src/main/java/az/turing/leetcode/easy/TwoSum.java
@@ -0,0 +1,37 @@
+package az.turing.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] array = {5, 4, 6, 9};
+        System.out.println(Arrays.toString(twoSumWithFor(array, 10)));
+        int[] array2 = {1, 10, 7, 4};
+        System.out.println(Arrays.toString(twoSumWithMap(array2, 11)));
+    }
+
+    public static int[] twoSumWithFor(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("Element not found into array");
+    }
+
+    public static int[] twoSumWithMap(int[] array, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {//5,4,6,9
+            int complement = target - array[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(array[i], i);
+        }
+        throw new IllegalArgumentException("Element not found into array");
+    }
+}
